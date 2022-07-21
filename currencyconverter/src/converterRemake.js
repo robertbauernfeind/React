e06c6b82532ed6584223776eba61ec0d21026696
Robert Bauernfeind <70053333+aCoder06@users.noreply.github.com>
2022-07-21 15:03:28 +0200

    fixed data request temporary

--- currencyconverter/src/converterRemake.js
+++ currencyconverter/src/converterRemake.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import "./css/converterRemake.css";
-import { Button, Card, CardContent, CardHeader, Alert, Collapse, IconButton, ThemeProvider, createTheme, TextField } from '@mui/material';
+import { Button, Card, CardContent, CardHeader, Alert, Collapse, IconButton, ThemeProvider, createTheme, TextField, Autocomplete } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
 export function ConvPageRemake() {
     const [curr, setCurr] = useState({
         from: "",
         to: "",
-        convRate: null
+        convRate: null,
+        array: [],
+        selectedFrom: "",
+        selectedTo
     });
+
+    const makeAPICall = async () => {
+        try{
+            const url = "http://localhost:8080/data";
+            const res = await fetch(url, {mode:"cors"});
+            const data = await res.json();
+            
+            setCurr({array: data});
+        }
+        catch(e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        makeAPICall();
+    }, []);
 
     const handleTextChange = (event) => {
         switch(event.target.id) {
+            case "txtFrom":
         }
     }
+
     return(
         <>
             <Card sx={{
@@ -24,11 +46,25 @@ export function ConvPageRemake() {
             >
                 <CardHeader title="Currency Converter" subheader="Remake"/>
                 <CardContent>
-                    <TextField
-                      id="convFrom"
-                      label={curr.from}
-                      onChange={handleTextChange}
-                    />
+                    <div className='from'>
+                        <TextField
+                            id="txtFrom"
+                            label={curr.from}
+                            onChange={handleTextChange}
+                            className="txtFields"
+                            size="small"
+                            sx={{
+                                width: 75
+                            }}
+                        />
+                        
+                        <Autocomplete
+                            id='cbxFrom'
+                            options={curr.array}
+                            size="small"
+                            renderInput={(params) => <TextField {...params} label="From"/>}
+                        />
+                    </div>
                 </CardContent>
             </Card>
         </>
